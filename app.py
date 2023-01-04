da2326db949fe75b3de1cbf5c204de9c55193151
Akshaysat <akshay.satpaise99@gmail.com>
2023-01-04 17:39:59 +0530

    added secrets.toml

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import streamlit as st
 import requests
 import pandas as pd
 import datetime as dt
+from google.oauth2 import service_account
+from gsheetsdb import connect
 
 # set page config
 st.set_page_config(page_title="LearnApp", page_icon="favicon.png")
@@ -24,3 +26,28 @@ st.markdown(
     "<h2 style='text-align: center; color: black;'>LA Cohort Leaderboard</h2>",
     unsafe_allow_html=True,
 )
+
+# Create a connection object.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+conn = connect(credentials=credentials)
+
+# Perform SQL query on the Google Sheet.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
+
+
+sheet_url = st.secrets["private_gsheets_url"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+# Print results.
+for row in rows:
+    st.write(f"{row.Name} has a score of {round(row.Score)}")
