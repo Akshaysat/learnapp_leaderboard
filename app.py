0249dba511734ce8e1ac7b0b6734d8ea9b299607
Akshaysat <akshay.satpaise99@gmail.com>
2023-01-05 20:42:07 +0530

    updated app.py

--- app.py
+++ app.py
@@ -45,9 +45,21 @@ def run_query(query):
     return rows
 
 
-sheet_url = st.secrets["private_gsheets_url"]
+st.write("----")
+
+cohort_name = st.selectbox("Select the cohort", ["latd-02"])
+
+sheet_url = st.secrets[f"private_gsheets_url_{cohort_name}"]
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
 # Print results.
-for row in rows:
-    st.write(f"{row.Name} has a score of {round(row.Score)}")
+# for row in rows:
+#     st.write(f"{row.Name} has a score of {round(row.Score)}")
+
+df = pd.DataFrame(rows)[["User_ID", "Name", "Score"]]
+df.set_index("User_ID", inplace=True)
+
+st.write("")
+st.write("")
+st.subheader(f"Leaderboard for {cohort_name.upper()}")
+st._legacy_dataframe(df)
